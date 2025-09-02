7f773d67c10da19088811e3ecc7982eeff86dff5
Mohit Reddy <mohitreddy@Mohits-MacBook-Air.local>
2025-09-02 09:31:09 +0530

    initial setup

---
+++ src/test/java/in/edu/ssn/FarmToMarket/FarmToMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package in.edu.ssn.FarmToMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmToMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
